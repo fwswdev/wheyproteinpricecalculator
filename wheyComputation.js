7e25d4af87cab08242a73af0209c59831e934aea
jjp <jjp@jjp.com>
2017-07-02 09:32:42 -0500

    working. still in progress

--- wheyComputation.js
+++ wheyComputation.js
@@ -1,10 +1,15 @@
-﻿function ComputeWhey(dataIn) {
-    dataIn = {}
-    dataIn.weightOfTubInLbs = 25;
-    dataIn.gramspowderperserving = 30;
-    dataIn.price = 25
-    dataIn.gramsproteinperserving = 20
-    dataIn.caloriesPerServing = 2
+﻿function getValFromTextBox(txtBoxName) {
+    var s = document.getElementById(txtBoxName);
+    return s.value;
+}
+
+function ComputeWhey(dataIn) {
+    //dataIn = {}
+    //dataIn.weightOfTubInLbs = 25;
+    //dataIn.gramspowderperserving = 30;
+    //dataIn.price = 25
+    //dataIn.gramsproteinperserving = 20
+    //dataIn.caloriesPerServing = 2
     //console.log("test");
     //var p = document.getElementById("inpPrice").value;
     //console.log(p);
@@ -28,17 +33,19 @@
     return dataOut
 }
 
-function GetFormattedComputation()
-{
-    dataIn = {}
-    dataIn.weightOfTubInLbs = 25;
-    dataIn.gramspowderperserving = 30;
-    dataIn.price = 25
-    dataIn.gramsproteinperserving = 20
-    dataIn.caloriesPerServing = 2
+function GetFormattedComputation() {
+    dataIn = {};
+    dataIn.weightOfTubInLbs = getValFromTextBox("txtbxPpWeight");
+    dataIn.gramspowderperserving = getValFromTextBox("txtbxPowderPerServing");
+    dataIn.price = getValFromTextBox("txtbxPrice");
+    dataIn.gramsproteinperserving = getValFromTextBox("txtbxProteinPerServing");
+    dataIn.caloriesPerServing = getValFromTextBox("txtbxCalPerServing");
 
-    var dataOut = ComputeWhey(dataIn)
-    var NEWLINE = "\n"
+    var b = (dataIn.weightOfTubInLbs && dataIn.gramspowderperserving &&
+        dataIn.price && dataIn.gramsproteinperserving && dataIn.caloriesPerServing);
+
+    var dataOut = ComputeWhey(dataIn);
+    var NEWLINE = "\n";
     var strOut =
 "Grams of Protein Power Per Tub: " + dataOut.gramsProteinPerTub + NEWLINE +
 "Number of servings per tub: " + dataOut.numServingsPerTub + NEWLINE +
@@ -46,7 +53,6 @@ function GetFormattedComputation()
 "Price per gram of protein: " + dataOut.pricePerGramProtein + NEWLINE +
 "Price per Calorie serving: " + dataOut.pricePerCalorie + NEWLINE +
 "Total Calories Per Tub: " + dataOut.totalCaloriesTub;
-    //document.write(strOut);
 
     document.getElementById("txtOutput").value = strOut;
 
